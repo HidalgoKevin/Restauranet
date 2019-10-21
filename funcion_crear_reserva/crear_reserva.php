2f66057a8c6aac9f29ffa533fb160ef054d5d0d1
Juan Carlos <salazarjuancarlos2018@gmail.com>
2019-10-21 19:20:21 -0300

    funcion_crear_reserva_cliente

---
+++ funcion_crear_reserva/crear_reserva.php
@@ -0,0 +1,30 @@
+﻿<?php
+
+session_start();
+include_once('conexion_bd.php');
+$idcliente=3;
+$fechahora= $_POST['fechahora'];
+$cantidad= $_POST['cantidad'];
+
+$fechahora = explode(" ",$fechahora);
+list($fecha, $hora)=$fechahora;
+
+$result_datos= "INSERT INTO reservas (idcliente, fecha, hora,cantidad_personas) VALUES ('$idcliente', STR_TO_DATE(REPLACE('$fecha','/','.') ,GET_FORMAT(date,'USA')),'$hora','$cantidad')";
+$resultado_datos= mysqli_query($mysqli, $result_datos);
+
+/*if(mysqli_query($mysqli, $result_datos)){
+	echo 'guardado con exito';
+}else{
+	echo 'guardado sin exito!';
+}*/
+
+if(mysqli_insert_id($mysqli)){
+	$_SESSION['msg']= "<div class='alert alert-success'> La Reserva se realizo con exito!</div>";
+	header("Location: Formulario_crear_reserva.php");
+}else{
+	$_SESSION['msg']= "<div class='alert alert-danger'> Error al realizar la Reserva!</div>";
+	header("Location: Formulario_crear_reserva.php");
+}
+
+
+?>
