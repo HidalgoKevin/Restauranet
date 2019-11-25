830ce6f9efcb4b1956fffb5357b9e52acb4faf4a
Juan Carlos <salazarjuancarlos2018@gmail.com>
2019-11-25 16:50:13 -0300

    Historial de calificaciones
    codigo para listar el historial de calificaciones.

---
+++ historial_calificaciones/mostrar_historial.php
@@ -0,0 +1,28 @@
+
+<?php
+
+$connect = mysqli_connect("localhost", "root", "", "app_rest");
+
+$idcliente=3;
+
+$query = "SELECT nombre, comentario, estrellas FROM restaurante R INNER JOIN calificaciones C on R.ID_RES = C.ID_RES
+WHERE C.ID_US ='$idcliente' ";
+
+
+$result = mysqli_query($connect, $query);
+
+echo '--------------------------------------------------------------------------------------------------------------------------------'.'<br>';
+echo 'ESTE ES SU HISTORIAL DE CALIFICACIONES: '.'<br>';
+echo '--------------------------------------------------------------------------------------------------------------------------------'.'<br><br>';
+
+while($row= mysqli_fetch_assoc($result))
+{		
+		echo "Restaurante:     ".$row['nombre'].'<br><br>';
+		echo 'Comentario: "...'.$row['comentario'].'..."'.'<br>';
+		echo 'Estrellas: '.$row['estrellas'].'<br><br>';
+		echo '---------------------------------------------------------------------------------------------------------------------------------'.'<br>';
+}
+
+echo '<br><br>'.'<input  calss="btn btn-primary" onClick="javascript:window.history.back();" type="button" name="Submit" value="Atrás" />';
+
+?>
